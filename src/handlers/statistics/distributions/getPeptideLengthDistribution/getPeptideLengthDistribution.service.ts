1ce61a0868b606be0ee82abecaec89487ca94e33
moonstar-x <christian.lopez99@outlook.com>
2024-08-20 18:10:21 -0500

    Implemented getPeptideLengthDistribution.

---
+++ src/handlers/statistics/distributions/getPeptideLengthDistribution/getPeptideLengthDistribution.service.ts
@@ -0,0 +1,14 @@
+import { GraphDatabaseService } from '../../../../shared/services/neo4j/GraphDatabaseService';
+
+export const getPeptideLengthDistribution = async (): Promise<Record<number, number>> => {
+  const db = GraphDatabaseService.getInstance();
+  const query = 'MATCH (n:Peptide) RETURN SIZE(n.seq) AS length, COUNT(*) as frequency ORDER BY length DESC';
+  const result = await db.query(query);
+
+  return Object.fromEntries(result.records.map((record) => {
+    const length = record.get('length').toInt();
+    const frequency = record.get('frequency').toInt();
+
+    return [length, frequency];
+  }));
+};
--- src/handlers/statistics/getPeptideCount/getPeptideCount.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { asyncHandler } from '../../../middleware/controller';
-import { get } from './getPeptideCount.controller';
-
-export const attach = (router: Router) => {
-  router.get('/statistics/peptide-count', asyncHandler(get));
-};
