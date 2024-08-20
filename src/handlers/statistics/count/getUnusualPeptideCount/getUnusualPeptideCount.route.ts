56b5bd98a05c042286abb0f608b8284267fe5512
moonstar-x <christian.lopez99@outlook.com>
2024-08-20 18:09:12 -0500

    Implemented getUnusualPeptideCount.

---
+++ src/handlers/statistics/count/getUnusualPeptideCount/getUnusualPeptideCount.route.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { asyncHandler } from '../../../../middleware/controller';
+import { get } from './getUnusualPeptideCount.controller';
+
+export const attach = (router: Router) => {
+  router.get('/statistics/count/unusual-peptides', asyncHandler(get));
+};
